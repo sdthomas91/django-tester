ab15a21640cc774c5a247db4c4750e3e38e9cb3e
Samuel Thomas <samueldthomas91@gmail.com>
2024-03-21 13:44:20 +0000

    corrects done model

--- todo/models.py
+++ todo/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.Boolean(null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False)
